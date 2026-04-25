75590e50f11e5cf877dcd92b764bf391226b5e16
hafizhboyensa <muhammadhafizhboyensa@gmail.com>
2026-04-25 20:25:12 +0700

    refactor: memindahkan delete  movie ke services

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -62,21 +62,12 @@ class MovieController extends Controller
     public function update(Request $request, $id)
     {
         $this->movieServices->updateMovie($request, $id);
-        return redirect('/')->with('success','Data berhasil disimpan');
+        return redirect('/movies/data')->with('success','Data berhasil disimpan');
     }
 
     public function delete($id)
     {
-        $movie = Movie::findOrFail($id);
-
-        // Delete the movie's photo if it exists
-        if (File::exists(public_path('images/' . $movie->foto_sampul))) {
-            File::delete(public_path('images/' . $movie->foto_sampul));
-        }
-
-        // Delete the movie record from the database
-        $movie->delete();
-
-        return redirect('/movies/data')->with('success', 'Data berhasil dihapus');
+        $this->movieServices->deleteMovie($id);
+        return redirect('/movies/data')->with('success','Data berhasil disimpan');
     }
 }
